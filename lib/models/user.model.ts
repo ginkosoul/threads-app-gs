512e79eb6fc3dda6e6896af4bbdf43172d3035bf
ginkosoul <ginkosoul@gmail.com>
2023-10-12 15:20:26 +0300

    much stuff have been added

---
+++ lib/models/user.model.ts
@@ -0,0 +1,27 @@
+import { Schema, model, models } from "mongoose";
+
+const userSchema = new Schema({
+  id: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  image: String,
+  bio: String,
+  threads: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Thread",
+    },
+  ],
+  onboarded: {
+    type: Boolean,
+    default: false,
+  },
+  communities: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Community",
+    },
+  ],
+});
+
+export const User = models.User || model("User", userSchema);
